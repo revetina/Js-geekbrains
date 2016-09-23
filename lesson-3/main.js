c7dd18b17ecfe4925ea7855573baad25d62bb3e8
revetina <katyuha.zoo@gmail.com>
2016-09-23 18:09:42 +0300

    Задание со * (шахматная доска).

--- lesson-3/main.js
+++ lesson-3/main.js
@@ -1,3 +1,5 @@
+
+//Цикл, который выводит треугольник
 var i;
 var triangl;
 triangl = "";
@@ -5,3 +7,19 @@ for (i = 1; i <= 7; i++) {
     triangl = triangl + "#";
     console.log(triangl);
 }
+
+//Шахматная доска
+var chess;
+var n;
+var chess="";
+for (n = 1; n <= 71; n++) {
+    if (n % 2 == 0 && n % 9 != 0){
+        chess = chess + "#";
+    }
+    if (n % 2 != 0 && n % 9 != 0){
+        chess = chess + " ";
+    }
+    if (n % 9 == 0)
+        chess = chess + "\n";
+}
+console.log(chess);
