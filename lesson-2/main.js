0b0dfcb7aeb0da9669e2e53f71e935e29578af80
revetina <katyuha.zoo@gmail.com>
2016-09-29 23:57:21 +0300

    поправила код

--- lesson-2/main.js
+++ lesson-2/main.js
@@ -1,11 +1,17 @@
 //первая часть домашки
-var a = 1, b = 1, c, d;
+var a = 1,
+    b = 1,
+    c, d;
 
-c = ++a; alert(c); // 2 сначала инкремент, затем выдает результат
-d = b++; alert(d); // 1 сначала результат, затем инкремент
+c = ++a;
+alert(c); // 2 сначала инкремент, затем выдает результат
+d = b++;
+alert(d); // 1 сначала результат, затем инкремент
 
-c = (2+ ++a); alert(c); // 5 к двойке прибавляется инкремент а (2+1)
-d = (2+ b++); alert(d); // 4 к двойке прибавляется b (2) и только потом она инкрементируется
+c = (2 + ++a);
+alert(c); // 5 к двойке прибавляется инкремент а (2+1)
+d = (2 + b++);
+alert(d); // 4 к двойке прибавляется b (2) и только потом она инкрементируется
 
 alert(a); // 3 два последовательных инкремента
 alert(b); // 3 тоже два инкремента
@@ -14,20 +20,21 @@ alert(b); // 3 тоже два инкремента
 //вторая часть домашки
 var a = 2;
 var x = 1 + (a *= 2);
-alert(x);// x = 5
+alert(x); // x = 5
 
 //задание со * вариант 1
 var number;
-    for(number = 1; number <= 100; number++){
-        if (number % 3 == 0 && number % 5 != 0){
-            console.log("fizz");
-        }if(number % 5 == 0 && number % 3 != 0){
-            console.log("buzz");
-        }if(number % 3 == 0 && number % 5 == 0){
-            console.log("fizzbuzz");
-        }
-        else {
-            console.log(number);
-        }
-            
+for (number = 1; number <= 100; number++) {
+    if (number % 3 == 0 && number % 5 != 0) {
+        console.log("fizz");
     }
+    if (number % 5 == 0 && number % 3 != 0) {
+        console.log("buzz");
+    }
+    if (number % 3 == 0 && number % 5 == 0) {
+        console.log("fizzbuzz");
+    } else {
+        console.log(number);
+    }
+
+}
