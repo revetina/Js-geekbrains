d3de44172b891b183808fe1a3c957372ab3f893e
revetina <katyuha.zoo@gmail.com>
2016-09-30 01:41:55 +0300

    вторая часть адресной книги

--- addressbook/main.js
+++ addressbook/main.js
@@ -17,16 +17,29 @@ var users = [{
     ]
 }];
 
-var user = {};
-
-user.firstName = prompt('Введите имя:');
-user.lastName = prompt('Введите фамилию:');
-user.birthDay = prompt('Дату рождения:');
-user.phones = [];
-user.phones[0] = prompt('Номер телефона:');
-
-users.push(user);
-
-for (var i = 0; i < users.length; ++i) {
-    console.log('Имя: ' + users[i].firstName, ' Фамилия: ' + users[i].lastName, ' Дата рождения: ' + users[i].birthDay, ' Телефоны: ' + users[i].phones.join(', '));
+function addUser(users) {
+    var user = {};
+    user.firstName = prompt('Введите имя:');
+    user.lastName = prompt('Введите фамилию:');
+    user.birthDay = prompt('Дату рождения:');
+    user.phones = [];
+    while (true) {
+        var phone = prompt('Номер телефона:(для выхода введите пустой номер)');
+        if (!phone) break;
+        user.phones.push(phone);
+    }
+    users.push(user);
 }
+
+function csvFormat(users) {
+    return users.map(function(user) {
+        return Object.keys(user).map(function(key) {
+            if (typeof user[key] == 'string' || typeof user[key] == 'number') return '"' + user[key] + '"';
+            else return user[key].map(function(phone) {
+                return '"' + phone + '"';
+            }).join(';');
+        }).join(';');
+    }).join('\r\n');
+}
+addUser(users);
+console.log(csvFormat(users));
