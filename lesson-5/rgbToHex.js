e886fe887c7c369cfe6d2d0131cdc971ec26d3ef
revetina <katyuha.zoo@gmail.com>
2016-10-01 11:36:42 +0300

    коментарии

--- lesson-5/rgbToHex.js
+++ lesson-5/rgbToHex.js
@@ -1,9 +1,9 @@
 function rgbToHex(r, g, b) {
     var massive = {};
-    massive.sl = "#";
-    massive.r = Number(r).toString(16).toUpperCase();
+    massive.sl = "#";//добавляет "#" перед выводом
+    massive.r = Number(r).toString(16).toUpperCase();//получает hex-значение, добавляет в массив
     massive.g = Number(g).toString(16).toUpperCase();
     massive.b = Number(b).toString(16).toUpperCase();
-    return (massive.sl + massive.r + massive.g + massive.b);
+    return (massive.sl + massive.r + massive.g + massive.b);//возвращает строковый массив
 }
 console.log(rgbToHex(prompt("r="), prompt("g="), prompt("b=")));
