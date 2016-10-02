8cdd0a7bad634af6bf4a4f38e254f49f0b943cd0
revetina <katyuha.zoo@gmail.com>
2016-10-02 19:33:44 +0300

    все-таки решила выводить переменную а не массив

--- lesson-5/rgbToHex.js
+++ lesson-5/rgbToHex.js
@@ -1,9 +1,12 @@
 function rgbToHex(r, g, b) {
-    var massive = {};
-    massive.sl = "#";//добавляет "#" перед выводом
-    massive.r = Number(r).toString(16).toUpperCase();//получает hex-значение, добавляет в массив
-    massive.g = Number(g).toString(16).toUpperCase();
-    massive.b = Number(b).toString(16).toUpperCase();
-    return (massive.sl + massive.r + massive.g + massive.b);//возвращает строковый массив
+    var hex;
+    m = Number(r).toString(16).toUpperCase(); //получает hex-значение
+    m = m.length > 1 ? m : "0" + m;
+    n = Number(g).toString(16).toUpperCase();
+    n = n.length > 1 ? n : "0" + n;
+    o = Number(b).toString(16).toUpperCase();
+    o = o.length > 1 ? o : "0" + o;
+    hex = "#" + m + n + o;
+    return (hex);
 }
 console.log(rgbToHex(prompt("r="), prompt("g="), prompt("b=")));
