150f61fbb5f87955ac9f696155d785e70c731890
revetina <katyuha.zoo@gmail.com>
2016-10-06 13:13:10 +0300

    делает список через цикл

--- lesson-6/arrayToList.js
+++ lesson-6/arrayToList.js
@@ -1,24 +1,24 @@
-function arrayToList(arr) {
-    function prepend(element) { //получает элемент и создаёт новый список, где этот элемент добавлен спереди к первоначальному списку
-        var newList = {
-            value: element,
+function arrayToList(arr) { //получает массив и преобразует в список
+    var list = null;
+    for (var i = arr.length - 1; i >= 0; i--) {
+        list = {
+            value: arr[i],
             rest: list
-        };
-        return newList;
-    };
-    var list = {
-        value: arr[0],
-        rest: {
-            value: arr[1],
-            rest: {
-                value: arr[2],
-                rest: null
-            }
         }
-    };
-    var list1 = prepend(0);
-    var list2 = prepend(-1);
-    console.log(list, list1, list2);
-};
+    }
+    return list;
+}
 
-arrayToList([1, 2, 3]);
+console.log(arrayToList([1, 2, 3]));
+
+
+function prepend(list, element) { //получает список и элемент, добавляет элемент к первоначальному списку.
+    var newList = {
+        value: element,
+        rest: list
+    };
+    return newList;
+};
+var list = arrayToList([1, 2, 3]);
+console.log(prepend(list, 0));
+console.log(prepend(list, -1));
